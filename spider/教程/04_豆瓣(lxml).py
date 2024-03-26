185975222d19cc00f41b215c9bed1d70c23bef32
haveadreammm <1090990640@qq.com>
2024-03-26 17:24:44 +0800

    03/26

--- spider/教程/04_豆瓣(lxml).py
+++ spider/教程/04_豆瓣(lxml).py
@@ -1,4 +1,5 @@
 import requests
+import time
 from lxml import etree
 
 
@@ -13,9 +14,34 @@ def get_html(url):
         return
 
 
+def get_info(html):
+    html = etree.HTML(html)
+    infos = html.xpath("//tr[@class='item']")
+    for info in infos:
+        # 书名
+        name = info.xpath('./td[2]/div[1]/a/text()')[0].strip()
+        # 作者 出版社 出版时间 价格
+        public_info = info.xpath('./td[2]/p[1]/text()')[0].strip()
+        # 评分
+        score = info.xpath('./td[2]/div[2]/span[2]/text()')[0].strip()
+        # 评级人数
+        comment_count = info.xpath('./td[2]/div[2]/span[3]/text()')[0].strip()
+
+        data = {
+            "书名": name,
+            "出版信息": public_info,
+            "评分": score,
+            "评价人数": comment_count
+        }
+        print(data)
+
+
 def main():
-    url =
-    html = get_html(url)
+    urls = ["https://book.douban.com/top250?start={}".format(str(i)) for i in range(0, 226, 25)]
+    for url in urls:
+        html = get_html(url)
+        get_info(html)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
